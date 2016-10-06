7b474fa038efe4f0d2b99901910c85149b77e707
Thithi32 <thierry@thde.pro>
2016-10-05 21:35:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ $Forn->execute();
     <nav class="navbar navbar-static-top">
       <div class="container">
         <div class="navbar-header">
-          <a href="index2.html" class="navbar-brand"><b>KD</b>Vacina</a>
+          <a href="index2.html" class="navbar-brand"><img src="./logo_cadevacina.png" /></a>
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
             <i class="fa fa-bars"></i>
           </button>
